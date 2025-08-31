e538df67b29c025b535e10ef4017a0c6ec59a641
wanyun <2843638389@qq.com>
2025-08-31 11:01:32 +0800

    利用互斥锁解决商品查询的缓存击穿问题

--- src/main/java/com/hmdp/constant/RedisConstants.java
+++ src/main/java/com/hmdp/constant/RedisConstants.java
@@ -11,6 +11,7 @@ public class RedisConstants {
 
     public static final Long CACHE_NULL_TTL = 2L;
 
+
     public static final Long CACHE_SHOP_TTL = 30L;
 
     public static final String LOCK_SHOP_KEY = "lock:shop:";
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -29,19 +29,74 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private StringRedisTemplate stringRedisTemplate;
 
     public Result queryById(Long id) {
+        //解决缓存穿透
+        //   Shop shop = queryWithPassThrough(id);
+
+        //解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+        return Result.ok(shop);
+    }
+
+    private Shop queryWithMutex(Long id) {
         String key = RedisConstants.CACHE_SHOP_KEY + id;
         //1.从redis查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         //2.判断是否存在
         if (shopJson != null && shopJson.length() != 0) {
             //3.存在，直接返回
-            Shop shop = JSON.parseObject(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSON.parseObject(shopJson, Shop.class);
         }
         //判断缓存的是否为空字符串
         //直接用==""判断不行，因为这个比较的是引用地址，而且shopJson可能为null造成NullPointerException
         if ("".equals(shopJson)) {
-            return Result.fail("店铺不存在");
+            return null;
+        }
+        // 4.实现缓存重建
+        Shop shop = null;
+        try {
+            //4.1 获取互斥锁
+            boolean trylock = trylock(RedisConstants.LOCK_SHOP_KEY + id);
+            // 4.2 判断否获取成功
+            if (!trylock) {
+                //4.3 失败，则休眠重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            //4.4 成功，根据id查询数据库
+            shop = getById(id);
+            //5.判断在数据库中是否存在
+            if (shop == null) {
+                //将空字符串写入redis
+                stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            //6.存在，写入redis
+            stringRedisTemplate.opsForValue().set(key, JSON.toJSONString(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //7.释放互斥锁
+            unLock(RedisConstants.LOCK_SHOP_KEY + id);
+        }
+        return shop;
+    }
+
+    private Shop queryWithPassThrough(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        //1.从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //2.判断是否存在
+        if (shopJson != null && shopJson.length() != 0) {
+            //3.存在，直接返回
+            return JSON.parseObject(shopJson, Shop.class);
+        }
+        //判断缓存的是否为空字符串
+        //直接用==""判断不行，因为这个比较的是引用地址，而且shopJson可能为null造成NullPointerException
+        if ("".equals(shopJson)) {
+            return null;
         }
         //4.不存在，根据id查询数据库
         Shop shop = getById(id);
@@ -49,13 +104,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (shop == null) {
             //将空字符串写入redis
             stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("店铺不存在");
+            return null;
         }
         //6.存在，写入redis
         stringRedisTemplate.opsForValue().set(key, JSON.toJSONString(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return Result.ok(shop);
+        return shop;
     }
 
+    private boolean trylock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", RedisConstants.LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        return flag != null && flag;
+    }
+
+    private void unLock(String key) {
+        stringRedisTemplate.delete(key);
+    }
 
     @Transactional
     public Result update(Shop shop) {
